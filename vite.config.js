81481504197a0d311cdfc9278bec642364ea07fc
Santiago S. Foschiatti <sanfosx@gmail.com>
2023-07-01 07:10:34 -0300

    vite config para githubpages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/tp3_info_react_rick_and_morty/"
+  base:"https://sanfosx.github.io/tp3_info_react_rick_and_morty/"
 })
