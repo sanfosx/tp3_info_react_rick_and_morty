cdc19ad0553e880b6e1487c9691a2cd8d13bc182
Santiago S. Foschiatti <sanfosx@gmail.com>
2023-07-05 13:27:06 -0300

    Agrego el componente loading

---
+++ src/components/Loading/Loading.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import 'loading.css'
+
+const Loading = () => {
+  return (
+    <div className="loading-container">
+    <div className="loading-spinner"></div>
+    <p>Cargando...</p>
+  </div>
+  )
+}
+
+export default Loading
